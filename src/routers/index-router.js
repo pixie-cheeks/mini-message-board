998d7771c5cc546064f13ed45b191f54797ba6f2
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2025-05-26 17:39:27 +0530

    Improve error handling

--- src/routers/index-router.js
+++ src/routers/index-router.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { getIndex, getUser } from '../controllers/index-controller.js';
+import { CustomNotFoundError } from '../errors.js';
 
 const indexRouter = Router();
 
 indexRouter.get('/', getIndex);
-indexRouter.get('/:id', getUser);
+indexRouter.get('/id/:id', getUser);
+indexRouter.get('/*all', (_req, _res, next) => {
+  next(new CustomNotFoundError('Page not found'));
+});
 
 export { indexRouter };
