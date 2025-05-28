b48dbb08720159261f6f7b822d5ca6e5153a5b83
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2025-05-28 20:31:31 +0530

    Make database swappable with a server db

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,6 +1,15 @@
 import 'dotenv/config';
 import { Pool } from 'pg';
 
-const pool = new Pool();
+const getConfig = () =>
+  process.env.DB_ENV === 'prod'
+    ? {
+        ssl: {
+          rejectUnauthorized: true,
+          ca: process.env.DB_SSL_CA,
+        },
+      }
+    : {};
+const pool = new Pool(getConfig());
 
 export { pool };
